f5c3f598c590622902a1423768f53250038418d7
EggBug75 <84621182+EggBug75@users.noreply.github.com>
2023-02-14 14:39:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("niechce").onmouseover = function()
+    {
+        document.getElementById('nie').style.display = 'none';
+        
+        document.getElementById('nie2').style.display = 'inline';
+    }
