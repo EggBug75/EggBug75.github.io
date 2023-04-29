4035f5debf73e5b276aceeb36a63c407c9fe0ef1
EggBug75 <84621182+EggBug75@users.noreply.github.com>
2023-04-30 01:11:37 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("niechce").onmouseover = function()
-    {
-        document.getElementById('nie').style.display = 'none';
-        
-        document.getElementById('nie2').style.display = 'inline';
-    }
